3a9c2ed1d462e9fb980e539ddbcc189a33e46157
Sottam Sikidar <197910936+sikidarSottam@users.noreply.github.com>
2025-05-25 07:03:02 +0000

    Edited accordingly

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
 
-};
+
+    if ((n1 > 0 && n2 > 0) && typeof n1 === "number" && Number.isInteger(n1) && typeof n2 === "number" && Number.isInteger(n2)) {
+        let s = 0;
+        if (n1 < n2) {
+            for (let x = n1; x <= n2; x++) {
+                s += x;
+            }
+        } else {
+            for (let x = n2; x <= n1; x++) {
+                s += x;
+            }
+        }
+        return s
+    };
+    return 'ERROR'
+}
 
 // Do not edit below this line
 module.exports = sumAll;
