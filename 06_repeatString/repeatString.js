a122d22eef534f1fdacb171fa368f25e63ef05f9
Sottam Sikidar <197910936+sikidarSottam@users.noreply.github.com>
2025-05-25 04:32:12 +0000

    Edited accordingly

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0){
+        return "ERROR"
+    }
+
+    let ans = ""
+    for (let i = 0; i < num; i++) {
+        ans = ans + str;
+    }
+    return ans;
 
 };
 
