b0383899e4db1abfd81bd5cbe170169e79154df1
Sottam Sikidar <197910936+sikidarSottam@users.noreply.github.com>
2025-05-25 07:10:07 +0000

    Edited accordingly

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,9 @@
-const leapYears = function() {
+const leapYears = function(num) {
+
+    if((num % 400 == 0) || (num % 4 == 0 && num % 100 != 0) ){
+        return true
+    }
+    return false
 
 };
 
