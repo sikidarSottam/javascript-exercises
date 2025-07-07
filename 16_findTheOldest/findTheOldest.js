26fbe11f4a23e6a08d605e72533e0b196281a22e
Sottam Sikidar <197910936+sikidarSottam@users.noreply.github.com>
2025-07-07 14:00:48 +0000

    Edit the js file

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
+    
+    let oldest = arr[0];
+    let maxAge = getAge(oldest, currentYear);
+    
+    for (let i = 1; i < arr.length; i++) {
+        const currentAge = getAge(arr[i], currentYear);
+        if (currentAge > maxAge) {
+            maxAge = currentAge;
+            oldest = arr[i];
+        }
+    }
+    
+    return oldest;
 };
+
+// Helper function to calculate age
+function getAge(person, currentYear) {
+    const deathYear = person.yearOfDeath || currentYear;
+    return deathYear - person.yearOfBirth;
+}; 
 
 // Do not edit below this line
 module.exports = findTheOldest;
