f8fc80dffb19aaa895ffd0a17f19eddba9dce81a
Sottam Sikidar <197910936+sikidarSottam@users.noreply.github.com>
2025-07-07 09:57:39 +0000

    Edit the code

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let s = 0
+  for (let i = 0; i < arr.length; i++) {
+    s += arr[i];
+  }
+  return s
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let s = 1
+  for (let i = 0; i < arr.length; i++) {
+    s *= arr[i];
+  }
+  return s
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+ return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+let s = 1
+for (let i = 1; i <= a; i++) {
+  s *= i  
+}
+return s
 };
 
 // Do not edit below this line
