3412977b0743468f5cf684c793efe512a6ad5c17
Sottam Sikidar <197910936+sikidarSottam@users.noreply.github.com>
2025-07-07 10:46:59 +0000

    Edit the code

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let ans = (x - 32) * (5 /9)
+  return ans.toFixed(2)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let ans = (x * 9/5) + 32)
+  return ans.toFixed(2)
 };
 
 // Do not edit below this line
