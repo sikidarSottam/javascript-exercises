3b219410a3e1a26ba7243f3157ca2d1656b5dc40
Sottam Sikidar <197910936+sikidarSottam@users.noreply.github.com>
2025-07-07 10:11:47 +0000

    Edit the code

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (abc) {
+    let str = abc.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str.charAt(i) != str.charAt(str.length - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
